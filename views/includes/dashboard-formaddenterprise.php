32925baff9d89f75766036af296ba0e64ec8df7f
ranp20 <polancoranp2019@gmail.com>
2022-04-18 20:07:04 -0500

    show form to add enterprise

--- views/includes/dashboard-formaddenterprise.php
+++ views/includes/dashboard-formaddenterprise.php
@@ -1,5 +1,14 @@
 <div class="cformAddAccountEnterprise">
 	<form method="POST" class="cformAddAccountEnterprise--form" id="form-AddAccountEnterprise">
+		<div class="cformAddAccountBank--form--cBtnClose">
+			<span id="icon_frmbtnCloseFrmEnterprise">
+				<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="16px" height="16px" version="1.1" viewBox="0 0 700 700">
+				 <g>
+				  <path d="m535.61 94.387c-102.45-102.45-268.78-102.45-371.23 0-102.45 102.45-102.45 268.78 0 371.23 102.45 102.45 268.78 102.45 371.23 0 102.45-102.45 102.45-268.78 0-371.23zm-24.746 24.746c88.785 88.785 88.785 232.95 0 321.74-88.785 88.785-232.95 88.785-321.74 0s-88.785-232.95 0-321.74c88.785-88.785 232.95-88.785 321.74 0zm-185.61 160.87-68.199-68.199c-6.832-6.8242-6.832-17.922 0-24.746 6.8242-6.832 17.922-6.832 24.746 0l68.199 68.199 68.199-68.199c6.8242-6.832 17.922-6.832 24.746 0 6.832 6.8242 6.832 17.922 0 24.746l-68.199 68.199 68.199 68.199c6.832 6.8242 6.832 17.922 0 24.746-6.8242 6.832-17.922 6.832-24.746 0l-68.199-68.199-68.199 68.199c-6.8242 6.832-17.922 6.832-24.746 0-6.832-6.8242-6.832-17.922 0-24.746z" fill-rule="evenodd"/>
+				 </g>
+				</svg>
+			</span>
+		</div>
 		<div class="cformAddAccountEnterprise--form--cTitle">
 			<h3 class="cformAddAccountEnterprise--form--cTitle--title">Agrega tu perfil de empresa</h3>
 		</div>
--- views/js/actions_pages/control-panel.js
+++ views/js/actions_pages/control-panel.js
@@ -1,5 +1,25 @@
 $(() => {
 	list_typeAccountProfile();
+	// ------------ MOSTRAR/OCULTAR EL FORMULARIO DE REGISTRO DE CUENTAS BANCARIAS
+	const btn_frmOpenAddAccountEnterprise = document.querySelector("#btn-addAccountEnterpriseShow");
+	const btn_frmCloseAddAccountEnterprise = document.querySelector("#icon_frmbtnCloseFrmEnterprise");
+	const c_totalfrmAddAccountEnterprise = document.querySelector(".cformAddAccountEnterprise");
+	const c_containfrmAddAccountEnterprise = document.querySelector("#form-AddAccountEnterprise");
+
+	btn_frmOpenAddAccountEnterprise.addEventListener("click", function(){
+		c_totalfrmAddAccountEnterprise.classList.add("show");
+		c_containfrmAddAccountEnterprise.classList.add("show");
+	});
+	btn_frmCloseAddAccountEnterprise.addEventListener("click", function(){
+		c_totalfrmAddAccountEnterprise.classList.remove("show");
+		c_containfrmAddAccountEnterprise.classList.remove("show");
+	});
+	c_totalfrmAddAccountEnterprise.addEventListener('click', e => {
+		if(e.target === c_totalfrmAddAccountEnterprise){
+			c_totalfrmAddAccountEnterprise.classList.remove('show');
+			c_containfrmAddAccountEnterprise.classList.remove("show");
+		}
+	});
 });
 
 function list_typeAccountProfile(){
@@ -9,16 +29,18 @@ function list_typeAccountProfile(){
 
 	tmpListUser += `<li class='cControlP__cont--containDash--c__cBtnsOpts-m--item'>
 			<a href='welcome' class='cControlP__cont--containDash--c__cBtnsOpts-m--link'>
-				<img src='${pathuser}' alt='' width="100" height="100" style="width=100%;height:auto;">
+				<img src='${pathuser}' alt='' width="100" height="100" style="width:100%;height:auto;">
 			</a>
 			<span>Juan Carlos Oscar Torres</span>
 		</li>
 		<li class='cControlP__cont--containDash--c__cBtnsOpts-m--item'>
 			<button type='button' id='btn-addAccountEnterpriseShow' class='cControlP__cont--containDash--c__cBtnsOpts-m--link'>
-				<img src='views/assets/img/svg/add-profile.svg' alt='' width="100" height="100" style="width=100%;height:auto;">
+				<img src='views/assets/img/svg/add-profile.svg' alt='' width="100" height="100" style="width:auto;height:auto;">
 			</button>
 			<span>Agregar empresa</span>
 		</li>`;
 
 	$("#c_listTypeProfileOfUser").html(tmpListUser);
 };
+
+

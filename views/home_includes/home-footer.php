6059185282a79f1b5bb71b3b63a76bdb0a6b7845
ranp20 <polancoranp2019@gmail.com>
2022-05-05 15:27:00 -0500

    repair styles into policy privacy

--- views/home_includes/home-footer.php
+++ views/home_includes/home-footer.php
@@ -3,37 +3,61 @@
 		<div class="cFooter--contT--c box">
 			<section class="cFooter--contT--c--sec">
 				<div class="cFooter--contT--c--sec--c">
-					<img class="cFooter--contT--c--sec--c--img" src="./views/assets/img/svg/logo-white.svg" alt="" width="100" height="100">
+					<img class="cFooter--contT--c--sec--c--img" src="<?= $url; ?>views/assets/img/svg/logo-white.svg" alt="" width="100" height="100">
 					<p class="cFooter--contT--c--sec--c--text">Somos la startup financiera que revoluciona la forma de administrar el dinero a través de una plataforma 100% digital.</p>
 					<div class="cFooter--contT--c--sec--c--cIcon">
-						<a href="#" class="cFooter--contT--c--sec--c--cIcon--link"><img class="cFooter--contT--c--sec--c--cIcon--link--icon" src="./views/assets/img/icons/instagram.svg" alt="" width="100" height="100"></a>
-						<a href="#" class="cFooter--contT--c--sec--c--cIcon--link"><img class="cFooter--contT--c--sec--c--cIcon--link--icon" src="./views/assets/img/icons/facebook.svg" alt="" width="100" height="100"></a>
-						<a href="#" class="cFooter--contT--c--sec--c--cIcon--link"><img class="cFooter--contT--c--sec--c--cIcon--link--icon" src="./views/assets/img/icons/linkedin.svg" alt="" width="100" height="100"></a>
+						<a href="javascript:void(0);" class="cFooter--contT--c--sec--c--cIcon--link">
+							<img class="cFooter--contT--c--sec--c--cIcon--link--icon" src="<?= $url; ?>views/assets/img/icons/instagram.svg" alt="" width="100" height="100">
+						</a>
+						<a href="javascript:void(0);" class="cFooter--contT--c--sec--c--cIcon--link">
+							<img class="cFooter--contT--c--sec--c--cIcon--link--icon" src="<?= $url; ?>views/assets/img/icons/facebook.svg" alt="" width="100" height="100">
+						</a>
+						<a href="javascript:void(0);" class="cFooter--contT--c--sec--c--cIcon--link">
+							<img class="cFooter--contT--c--sec--c--cIcon--link--icon" src="<?= $url; ?>views/assets/img/icons/linkedin.svg" alt="" width="100" height="100">
+						</a>
 					</div>
 				</div>
 				<div class="cFooter--contT--c--sec--c">
 					<h3 class="cFooter--contT--c--sec--c--title">Menú</h3>
 					<ul class="cFooter--contT--c--sec--c--m">
-						<li class="cFooter--contT--c--sec--c--m--item"><a href="" id="" class="cFooter--contT--c--sec--c--m--link">Nosotros</a></li>
-						<li class="cFooter--contT--c--sec--c--m--item"><a href="" id="" class="cFooter--contT--c--sec--c--m--link">Contacto</a></li>
-						<li class="cFooter--contT--c--sec--c--m--item"><a href="" id="" class="cFooter--contT--c--sec--c--m--link">Preguntas frecuentes</a></li>
-						<li class="cFooter--contT--c--sec--c--m--item"><a href="" id="" class="cFooter--contT--c--sec--c--m--link"><img src="./views/assets/img/icons/whatsapp.svg" alt="" width="100" height="100">Atención al usuario</a></li>
+						<li class="cFooter--contT--c--sec--c--m--item">
+							<a href="javascript:void(0);" id="" class="cFooter--contT--c--sec--c--m--link">Nosotros</a>
+						</li>
+						<li class="cFooter--contT--c--sec--c--m--item">
+							<a href="javascript:void(0);" id="" class="cFooter--contT--c--sec--c--m--link">Contacto</a>
+						</li>
+						<li class="cFooter--contT--c--sec--c--m--item">
+							<a href="javascript:void(0);" id="" class="cFooter--contT--c--sec--c--m--link">Preguntas frecuentes</a>
+						</li>
+						<li class="cFooter--contT--c--sec--c--m--item">
+							<a href="javascript:void(0);" id="" class="cFooter--contT--c--sec--c--m--link"><img src="<?= $url; ?>views/assets/img/icons/whatsapp.svg" 
+								alt="" width="100" height="100">Atención al usuario</a></li>
 					</ul>
 				</div>
 				<div class="cFooter--contT--c--sec--c">
 					<h3 class="cFooter--contT--c--sec--c--title">Legal</h3>
 					<ul class="cFooter--contT--c--sec--c--m">
-						<li class="cFooter--contT--c--sec--c--m--item"><a href="" id="" class="cFooter--contT--c--sec--c--m--link">Términos y condiciones</a></li>
+						<li class="cFooter--contT--c--sec--c--m--item">
+							<a href="javascript:void(0);" id="" class="cFooter--contT--c--sec--c--m--link">Términos y condiciones</a>
+						</li>
 						<li class="cFooter--contT--c--sec--c--m--item"><a href="politicas-de-privacidad" id="" class="cFooter--contT--c--sec--c--m--link">Políticas de privacidad</a></li>
-						<li class="cFooter--contT--c--sec--c--m--item"><a href="" id="" class="cFooter--contT--c--sec--c--m--link">Libro de reclamaciones</a></li>
+						<li class="cFooter--contT--c--sec--c--m--item">
+							<a href="javascript:void(0);" id="" class="cFooter--contT--c--sec--c--m--link">Libro de reclamaciones</a>
+						</li>
 					</ul>
 				</div>
 				<div class="cFooter--contT--c--sec--c">
 					<h3 class="cFooter--contT--c--sec--c--title">Datos de contacto</h3>
 					<ul class="cFooter--contT--c--sec--c--m">
-						<li class="cFooter--contT--c--sec--c--m--item"><a href="" id="" class="cFooter--contT--c--sec--c--m--link">contacto@micambista.net</a></li>
-						<li class="cFooter--contT--c--sec--c--m--item"><a href="" id="" class="cFooter--contT--c--sec--c--m--link">Lunes a Viernes 9 a.m - 7 p.m</a></li>
-						<li class="cFooter--contT--c--sec--c--m--item"><a href="" id="" class="cFooter--contT--c--sec--c--m--link">Sábados y Feriados 9 a.m - 2.30 p.m</a></li>
+						<li class="cFooter--contT--c--sec--c--m--item">
+							<a href="javascript:void(0);" id="" class="cFooter--contT--c--sec--c--m--link">contacto@micambista.net</a>
+						</li>
+						<li class="cFooter--contT--c--sec--c--m--item">
+							<a href="javascript:void(0);" id="" class="cFooter--contT--c--sec--c--m--link">Lunes a Viernes 9 a.m - 7 p.m</a>
+						</li>
+						<li class="cFooter--contT--c--sec--c--m--item">
+							<a href="javascript:void(0);" id="" class="cFooter--contT--c--sec--c--m--link">Sábados y Feriados 9 a.m - 2.30 p.m</a>
+						</li>
 					</ul>
 				</div>
 			</section>

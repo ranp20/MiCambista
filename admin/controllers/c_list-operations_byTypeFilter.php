a857d1c0b5beb2f995cb6b1d063178cd031ba8cc
ranp20 <polancoranp2019@gmail.com>
2022-05-15 19:55:26 -0500

    repair send of params into query to filter operations into admin

--- admin/controllers/c_list-operations_byTypeFilter.php
+++ admin/controllers/c_list-operations_byTypeFilter.php
@@ -4,19 +4,24 @@ if(isset($_POST) && count($_POST) > 0){
 	$option = $_POST['option'];
 	$sql = "";
 
-	if($option == "Completed"){
-		$sql = "SELECT * FROM tbl_transactions ttrans WHERE ttrans.status_send = 'Completed' ORDER BY ttrans.id DESC";
-	}else if($option == "Pendings" || $option == "Processed"){
-		$sql = "SELECT * FROM tbl_transactions ttrans WHERE ttrans.status_send = 'Pending' ORDER BY ttrans.id DESC";
-	}else if($option == "Canceled"){
-		$sql = "SELECT * FROM tbl_transactions ttrans WHERE ttrans.status_send = 'Cancel' ORDER BY ttrans.id DESC";
+	if(isset($_POST['option']) && !empty($_POST['option'])){
+		if($option == "Completed"){
+			$sql = "SELECT * FROM tbl_transactions ttrans WHERE ttrans.status_send = 'Completed' ORDER BY ttrans.id DESC";
+		}else if($option == "Pendings" || $option == "Processed"){
+			$sql = "SELECT * FROM tbl_transactions ttrans WHERE ttrans.status_send = 'Pending' ORDER BY ttrans.id DESC";
+		}else if($option == "Canceled"){
+			$sql = "SELECT * FROM tbl_transactions ttrans WHERE ttrans.status_send = 'Cancel' ORDER BY ttrans.id DESC";
+		}else{
+			$sql = "SELECT * FROM tbl_transactions ttrans WHERE ttrans.status_send = 'Pending' ORDER BY ttrans.id DESC";
+		}
 	}else{
+		$option = "Pending";
 		$sql = "SELECT * FROM tbl_transactions ttrans WHERE ttrans.status_send = 'Pending' ORDER BY ttrans.id DESC";
 	}
 
 	require_once 'connection.php';
 	$stm = $con->prepare($sql);
-	$stm->bindValue("option", $option);
+	//$stm->bindValue("option", $option);
 	$stm->execute();
 	$data = $stm->fetchAll(PDO::FETCH_ASSOC);
 	if(isset($data) && !empty($data)){

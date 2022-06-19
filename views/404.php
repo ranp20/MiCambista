1eb81764dc9836e7e8e9d86aeca9ffc37afb341e
ranp20 <polancoranp2019@gmail.com>
2022-06-19 11:43:57 -0500

    repair page 404

---
+++ views/404.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Memopay |</title>
+	<link rel="stylesheet" href="views/assets/css/styles.min.css">
+</head>
+<body>
+	<div class="cErr-404_cont">
+		<section class="cErr-404_cont__sbanner"></section>
+		<section class="cErr-404_cont__desc">
+			<h1>404</h1>
+			<h2>No encontramos la página.</h2>
+			<p>Tal vez copiaste mal la direción. <span class="bold-pricolor">¡Hasta allá no llega Memopay!</span><br>Por favor, regresa.</p>
+			<a href="/Memopay">Regresar</a>
+		</section>
+	</div>
+</body>
+</html>

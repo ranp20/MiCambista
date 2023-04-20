e76936d193d4c5c784f36383a253786fe6965c7f
ranp20 <polancoranp2019@gmail.com>
2023-04-20 16:46:51 -0500

    repair changes into home

--- index.php
+++ index.php
@@ -14,6 +14,97 @@
 <head>
 	<?php require_once 'views/includes/header_links.php' ?> 
 	<title>Memopay | Centro de cambio en línea con las mejores tasas</title>
+	<style type="text/css">
+		.cSwitchTgg__scheme{
+	    display: inline-block;
+			position: fixed;
+	    bottom: 110px;
+	    right: 0px;
+	    background-color: #fefefe;
+	    z-index: 999;
+	    box-shadow: 0 2px 10px 1px rgba(0,0,0,.2);
+	    border-radius: 30px 0px 0px 30px;
+	    padding-top: 8px;
+	    padding-bottom: 8px;
+	    padding-left: 8px;
+	    padding-right: 8px;
+	    display: flex;
+	    align-items: center;
+	    justify-content: center;
+		}
+		.cSwitchTgg__scheme label {
+			width: 70px;
+			height: 30px;
+			position: relative;
+			display: block;
+			background: #ebebeb;
+			border-radius: 200px;
+			box-shadow: inset 0px 5px 15px rgba(0,0,0,0.4), inset 0px -5px 15px rgba(255,255,255,0.4);
+			cursor: pointer;
+			transition: 0.3s;
+		}
+		.cSwitchTgg__scheme label:after {
+			content: "";
+			width: 30px;
+			height: 30px;
+			position: absolute;
+			top: 0px;
+			left: 0px;
+			background: linear-gradient(180deg,#ffcc89,#d8860b);
+			border-radius: 180px;
+			box-shadow: 0px 5px 10px rgba(0,0,0,0.2);
+			transition: 0.3s;
+		}
+		.cSwitchTgg__scheme input {
+			width: 0;
+			height: 0;
+			visibility: hidden;
+		}
+		.cSwitchTgg__scheme input:checked + label {
+			background: #242424;
+		}
+		.cSwitchTgg__scheme input:checked + label:after {
+			left: 70px;
+			transform: translateX(-100%);
+			background: linear-gradient(180deg,#777,#3a3a3a);
+		}
+		.cSwitchTgg__scheme label:active:after{
+			width: 30px;
+		}
+		.background {
+			width:100vw;
+			height: 100vh;
+			background: #fff;
+			z-index: -1;
+			position: absolute;
+			transition: 0.3s;
+		}
+		.cSwitchTgg__scheme input:checked + label + .background {
+			background:#242424;
+		}
+		.cSwitchTgg__scheme label svg {
+			position: absolute;
+			width: 26px;
+			top: 4px;
+			z-index: 100;
+		}
+		.cSwitchTgg__scheme label svg.sun {
+			left: 1px;
+			fill: #fff;
+			transition: 0.3s;
+		}
+		.cSwitchTgg__scheme label svg.moon {
+			left: 42px;
+			fill:#7e7e7e;
+			transition: 0.3s;
+		}
+		.cSwitchTgg__scheme input:checked + label svg.sun {
+			fill:#7e7e7e;
+		}
+		.cSwitchTgg__scheme input:checked + label svg.moon {
+			fill:#fff;
+		}
+	</style>
 </head>
 <body>
 	<?php require_once 'views/includes/api_whatsapp.php';?>
@@ -239,8 +330,36 @@
 			<?php /*require_once 'views/home_includes/home-contact.php';*/ ?>
 		</div>
 	</main>
-	<?php require_once 'views/home_includes/home-footer.php'; ?>
+	<?php require_once 'views/home_includes/home-footer.php';?>
+	<div class="cSwitchTgg__scheme">
+		<input type="checkbox" id="darkmode-toggle"/>
+		<label for="darkmode-toggle">
+		   <svg xmlns="http://www.w3.org/2000/svg" class="sun" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" version="1.1" viewBox="0 0 700 700"><g xmlns="http://www.w3.org/2000/svg"><path d="m600.82 292.86c-7.0898 0-12.852-5.7617-12.852-12.852 0-7.1055 5.7617-12.852 12.852-12.852h10.633c7.1055 0 12.852 5.7617 12.852 12.852 0 7.1055-5.7617 12.852-12.852 12.852zm-250.83-105.59c-7.1055 0-12.852-5.7617-12.852-12.852 0-7.1055 5.7617-12.852 12.852-12.852 32.691 0 62.312 13.254 83.75 34.691s34.691 51.055 34.691 83.75c0 7.1055-5.7617 12.852-12.852 12.852-7.1055 0-12.852-5.7617-12.852-12.852 0-25.602-10.383-48.789-27.164-65.57-16.785-16.785-39.969-27.164-65.57-27.164zm-181.41 255.95c5.0234-5.0234 13.156-5.0234 18.18 0 5.0234 5.0234 5.0234 13.156 0 18.18l-33.414 33.434c-5.0234 5.0234-13.156 5.0234-18.18 0-5.0234-5.0234-5.0234-13.156 0-18.18l33.434-33.434zm344.66 18.18c-5.0234-5.0234-5.0234-13.156 0-18.18s13.156-5.0234 18.18 0l33.434 33.434c5.0234 5.0234 5.0234 13.156 0 18.18-5.0234 5.0234-13.156 5.0234-18.18 0zm18.18-344.66c-5.0234 5.0234-13.156 5.0234-18.18 0-5.0234-5.0234-5.0234-13.156 0-18.18l33.434-33.434c5.0234-5.0234 13.156-5.0234 18.18 0 5.0234 5.0234 5.0234 13.156 0 18.18l-33.434 33.414zm-344.66-18.18c5.0234 5.0234 5.0234 13.156 0 18.18-5.0234 5.0234-13.156 5.0234-18.18 0l-33.434-33.414c-5.0234-5.0234-5.0234-13.156 0-18.18 5.0234-5.0234 13.156-5.0234 18.18 0l33.414 33.434zm176.1-69.402c0 7.1055-5.7617 12.852-12.852 12.852-7.1055 0-12.852-5.7617-12.852-12.852v-10.617c0-7.1055 5.7617-12.852 12.852-12.852s12.852 5.7617 12.852 12.852zm-12.852 54.062c54.332 0 103.52 22.023 139.12 57.625 35.617 35.617 57.641 84.809 57.641 139.14s-22.023 103.52-57.641 139.14c-35.598 35.598-84.809 57.625-139.12 57.625-54.332 0-103.52-22.023-139.14-57.641-35.598-35.598-57.625-84.789-57.625-139.12 0-54.332 22.023-103.52 57.625-139.14 35.617-35.598 84.809-57.625 139.14-57.625zm120.96 75.82c-30.961-30.961-73.719-50.098-120.96-50.098-47.242 0-90 19.137-120.96 50.098-30.961 30.961-50.098 73.719-50.098 120.96 0 47.242 19.137 90 50.098 120.96 30.961 30.945 73.719 50.098 120.96 50.098 47.227 0 90-19.152 120.96-50.098 30.945-30.961 50.098-73.719 50.098-120.96 0-47.242-19.152-90-50.098-120.96zm-371.79 108.09c7.1055 0 12.852 5.7617 12.852 12.852 0 7.1055-5.7617 12.852-12.852 12.852h-10.617c-7.1055 0-12.852-5.7617-12.852-12.852 0-7.1055 5.7617-12.852 12.852-12.852zm237.97 263.68c0-7.0898 5.7617-12.852 12.852-12.852s12.852 5.7617 12.852 12.852v10.633c0 7.1055-5.7617 12.852-12.852 12.852-7.1055 0-12.852-5.7617-12.852-12.852z"/></g></svg>
+		   <svg xmlns="http://www.w3.org/2000/svg" class="moon" x="0px" y="0px" version="1.1" viewBox="0 0 700 700"><path d="m367.5 525c-150.5 0-262.5-112-262.5-262.5 0-147 108.5-245 210-245 7 0 12.25 3.5 15.75 8.75s3.5 12.25 0 17.5c-63 98-29.75 189 19.25 236.25 47.25 47.25 138.25 82.25 236.25 19.25 5.25-3.5 12.25-3.5 17.5 0s8.75 8.75 8.75 15.75c0 101.5-98 210-245 210zm-84-469c-73.5 19.25-143.5 98-143.5 206.5 0 129.5 98 227.5 227.5 227.5 108.5 0 187.25-70 206.5-143.5-98 45.5-192.5 14-248.5-42-56-54.25-87.5-150.5-42-248.5z"/></svg>
+		</label>
+	</div>
 	<script type="text/javascript" src="<?= $url ?>views/js/main.js"></script>
 	<script type="text/javascript" src="<?= $url ?>views/js/actions_pages/home_convert-divise.js"></script>
+	<script type="text/javascript">
+		var prefsColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+		var darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+		var switchColorScheme = document.querySelector('#darkmode-toggle');
+		const setColorScheme = (theme) => {
+			document.documentElement.setAttribute('data-theme', theme);
+			localStorage.setItem('theme', theme);
+		};
+
+		switchColorScheme.addEventListener('click', () => {
+			let switchToColor = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
+			setColorScheme(switchToColor);
+		});
+
+		setColorScheme(localStorage.getItem('theme') || prefsColorScheme);
+
+		darkQuery.addEventListener('change', function(e){
+			setColorScheme(e.matches ? "dark" : "light");
+		});
+	</script>
 </body>
 </html>
--- views/includes/header_links.php
+++ views/includes/header_links.php
@@ -17,7 +17,7 @@
 <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate"/>
 <meta http-equiv="Pragma" content="no-cache"/>
 <meta http-equiv="Expires" content="0"/>
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1.0, shrink-to-fit=no, viewport-fit=cover"/>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=6.0, minimum-scale=1.0, shrink-to-fit=no, viewport-fit=cover"/>
 <meta name="keywords" content="memopay, Casa de cambio online, Cambiar soles en Perú, Cambiar soles ahora"/>
 <meta name="description" content="¡Gana cambiando con Memopay! Dale a tu dinero el valor que merece."/>
 <meta name="theme-color" content="#FFF394"/>
